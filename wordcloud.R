9d98b84321ae23016db3752a26388fa4a62621d9
Morales <55636401+PrMorales@users.noreply.github.com>
2022-04-23 18:54:49 -0300

    Projeto Word Cloud
    Word Cloud desenvolvida com R a partir do artigo em PDF elaborado por alunos de Enfermagem da da Faculdade Aggeu Magalhães.
    Título: Pós covid-19: as sequelas deixadas pelo Sars-Cov-2 e o impacto na vida das pessoas acometidas.

---
+++ wordcloud.R
@@ -0,0 +1,37 @@
+library(pdftools)
+library(tidytext)
+library(dplyr)
+library(ggplot2)
+library(wordcloud)
+library("SnowballC")
+library(tm)
+
+
+text <- pdf_text('./artigo.pdf')
+text_df<- data_frame(line=1:15,text=text)
+
+#quebrar sentenÃ§as em tokens
+tidy_artigo <- text_df %>%
+  unnest_tokens(word,text)
+  
+  stopwords_pt <- c(stopwords("pt"), "de", '2675', "Ã©", 'bem', 'china', "sr", "sra",'ainda','meio','mai', "que", 'forma', 'academica',
+                    "a", "curitiba", "2020", 'silva','acadêmica', 'maio', 'revisão', 'é', 'pós', 'literatura', '1', 'além','podem', 'ser', 'além','casos', 'alÃ©m', 'campos', 'sobre', 'acadÃªmica', 'sendo',"2021", '457','4711','individuos', 'menos','2675', 'jun', 'cov', '471',"19","al","2","revisÃ£o", "pÃ³s","archives","sp", "v.exa")
+stopwords_pt_df <- data.frame(word = stopwords_pt)
+
+tidy_artigo <- tidy_artigo %>%
+  anti_join(stopwords_pt_df, by = "word")
+
+
+tidy_artigo %>% 
+  count(word, sort= TRUE)
+
+
+
+#wordcloud
+
+tidy_artigo %>%
+  anti_join(stop_words)%>%
+  count(word)%>%
+  with(wordcloud(word, n, max.words=50, colors=brewer.pal(7, "Dark2")))
+  
+
